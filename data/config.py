047819d845c9c6e72e459938d70d77d1f25d1c4b
Arima <137006062+Creator-Art-Life@users.noreply.github.com>
2024-07-05 10:58:26 +0300

    add project components

---
+++ data/config.py
@@ -0,0 +1,43 @@
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '' # API TOKEN get in @BotFather
+CHAT_ID = '1111111' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [5, 15]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+# скок поинтов с игры
+POINTS = [140, 175] #[min, max]
+
+# тратить алмазы
+SPEND_DIAMONDS = True # True/False
+
+# сон между играми
+SLEEP_GAME_TIME = [100,200] #[min,max]
+
+# мини задержки
+MINI_SLEEP = [3,7] #[min,max]
+
+# доп задержка после 8часов 
+SLEEP_8HOURS = [60*60,120*60] #[min,max] seconds
+
+hello = '''
+______ _                 _____        __ _   
+| ___ \ |               /  ___|      / _| |  
+| |_/ / |_   _ _ __ ___ \ `--.  ___ | |_| |_ 
+| ___ \ | | | | '_ ` _ \ `--. \/ _ \|  _| __|
+| |_/ / | |_| | | | | | /\__/ / (_) | | | |_ 
+\____/|_|\__,_|_| |_| |_\____/ \___/|_|  \__|
+
+'''
